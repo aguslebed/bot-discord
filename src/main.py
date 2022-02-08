8cd9a0039a097daabe27197c96f3d2fca9ea53f7
aguslebed <95714734+aguslebed@users.noreply.github.com>
2022-02-08 04:01:31 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ bot = commands.Bot(command_prefix='!', description='botsito discord')
 
 
 #Bot de discord
-TOKEN_DISCORD = 'OTE0OTYxMjEzODcwNDQ4Njcx.YaUqCA.hyxzdlOGFVMqYrPJcsr2OnPMUTk'
+TOKEN_DISCORD = 'OTE0OTYxMjEzODcwNDQ4Njcx.YaUqCA.2l59jK3Sj_cq2FbVa6PbRTlphf8'
 #api de la Nasa
 KEY = 'FTCG0fbOdWjTk6LFkKzYcR82NDEOQhHlhfQAfu8O'
 #api de Pexels
